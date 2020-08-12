e3948184c9f238f38d71cdc157bcd1a4dcb30758
Nikolas Lenning <nikolaslenning@gmail.com>
2020-08-12 14:46:17 -0600

    stopped function from running twice when clicked multiple times

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ let secondsLeft = 0;
 var startButton = document.getElementById('startButton');
 timeEl.textContent = "Time: " + secondsLeft;
 
-// Event listener that runs callback function to start timer at 75
+// Event listener that runs callback function setTime to start timer
 startButton.addEventListener('click', function setTime() {
+    // callback(startButton);
     var secondsLeft = 76
     var timerInterval = setInterval(function() {
     secondsLeft--;
     timeEl.textContent ="Time: " +  secondsLeft;
-
+    startButton.setAttribute('disabled', true);
+    // ^^^^^ solution to preventing double click running fuction setTime twice ----> https://stackoverflow.com/questions/20281546/how-to-prevent-calling-of-en-event-handler-twice-on-fast-clicks
+    
     if(secondsLeft === 0) {
       clearInterval(timerInterval);
       sendMessage();
